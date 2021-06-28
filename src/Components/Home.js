a22e34ecd9020a450b4569c9b740db030c77875c
Your NameNoémie <parisnanterre2021@gmail.com>
2021-06-28 14:21:36 +0200

    changereposive home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,8 +9,8 @@ const Home = () => {
                         <div>Hello, I’m  <span>Noémie Pham</span> </div>
                         <div>Front-end web developer </div>
                     </div>
-                    <div className ="viewbox">
-                        <p>VIEW MORE</p>
+                    <div className ="viewbox" >
+                        <a href ="#about">VIEW MORE</a>
                         <svg className ="arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 5v13M5 12l7 7 7-7"/></svg>
                     </div>
                 </div>
